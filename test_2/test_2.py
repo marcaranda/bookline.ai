7c450e4a90618b2aab0d0404f7f22afda1d4ffd0
marcaranda <marc.aranda@estudiantat.upc.edu>
2024-11-25 13:44:20 +0100

    rent car

--- test_2/test_2.py
+++ test_2/test_2.py
@@ -3,6 +3,8 @@ from datetime import date
 
 app = FastAPI()
 
+cars = ["TOYOTA", "BMW", "MERCEDES", "HYUNDAI"]
+rentedCars = []
 
 @app.get("/dateAvaiblesCars")
 def dateAvaiablesCars(date: date):
@@ -10,4 +12,8 @@ def dateAvaiablesCars(date: date):
 
 @app.post("/rentCar")
 def rentCar(car: str, date: date):
-    return
+    if (car in cars and car not in rentedCars[0]) or (car in cars and car in rentedCars[0] and date not in rentedCars[1]):
+        rentedCars.append((car, date))
+        return f"Car {car} rented successfully for {date.strftime("%Y-%m-%d")}"
+    else:
+        return f"Car {car} not available for {date.strftime("%Y-%m-%d")}"
