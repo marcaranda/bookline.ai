a3326483b95b014e8b89e926bcd4aa7adcf360c3
marcaranda <marc.aranda@estudiantat.upc.edu>
2024-11-25 14:50:57 +0100

    save rentedCars on database file

--- test_2/test_2.py
+++ test_2/test_2.py
@@ -1,21 +1,48 @@
 from fastapi import FastAPI
 from datetime import date
+import json
+import os
 
 app = FastAPI()
 
 cars = ["TOYOTA", "BMW", "MERCEDES", "HYUNDAI"]
-rentedCars = []
 
 @app.get("/dateAvaiblesCars")
 def dateAvaiablesCars(date: date):
-    avaiblesCars = [car for car in cars if car not in [rentedCar[0] for rentedCar in rentedCars if rentedCar[1] == date]]
-    return {f"Available cars for {date.strftime('%Y-%m-%d')}": avaiblesCars}
+    # If database.json contains the rented cars list, load it, else create an empty list
+    if os.stat("database.json").st_size > 0:
+      with open('database.json') as json_file:
+          rentedCarsList = json.load(json_file)
+    else:
+      rentedCarsList = []
+
+    avaiblesCars = [car for car in cars if car not in [rentedCar["car"] for rentedCar in rentedCarsList if rentedCar["rentalDate"] == date.strftime('%Y-%m-%d')]]
+    return {f"Available cars for {date}": avaiblesCars}
 
 @app.post("/rentCar")
 def rentCar(car: str, date: date):
+    # Check if the car is at the cars list, else return an error message
     if car in cars:
-      if car not in [rentedCar[0] for rentedCar in rentedCars if rentedCar[1] == date]:
-          rentedCars.append((car, date))
+      # If database.json contains the rented cars list, load it, else create an empty list
+      if os.stat("database.json").st_size > 0:
+        with open('database.json') as json_file:
+            rentedCarsList = json.load(json_file)
+      else:
+        rentedCarsList = []
+
+      # Check if the car is available for the date, else return an error message
+      if car not in [rentedCar["car"] for rentedCar in rentedCarsList if rentedCar["rentalDate"] == date.strftime('%Y-%m-%d')]:
+          # Append the new rented car to the list   
+          rentedCar = {
+              "car": car,
+              "rentalDate": date.strftime('%Y-%m-%d')
+          }
+          rentedCarsList.append(rentedCar)
+
+          # Save the rented cars list to database
+          with open('database.json', 'w') as json_file:
+              json.dump(rentedCarsList, json_file)
+
           return f"Car {car} rented successfully for {date.strftime('%Y-%m-%d')}"
       else:
           return f"Car {car} not available for {date.strftime('%Y-%m-%d')}"
